84ac1ec6caddbb2ce15e055999fc812e0462fc27
Trevis Thomas <trevisthomas@gmail.com>
2017-10-03 18:07:53 -0500

    J without regularization

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -8,8 +8,8 @@ function [J grad] = nnCostFunction(nn_params, ...
 %   [J grad] = NNCOSTFUNCTON(nn_params, hidden_layer_size, num_labels, ...
 %   X, y, lambda) computes the cost and gradient of the neural network. The
 %   parameters for the neural network are "unrolled" into the vector
-%   nn_params and need to be converted back into the weight matrices. 
-% 
+%   nn_params and need to be converted back into the weight matrices.
+%
 %   The returned parameter grad should be a "unrolled" vector of the
 %   partial derivatives of the neural network.
 %
@@ -24,8 +24,8 @@ Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):en
 
 % Setup some useful variables
 m = size(X, 1);
-         
-% You need to return the following variables correctly 
+
+% You need to return the following variables correctly
 J = 0;
 Theta1_grad = zeros(size(Theta1));
 Theta2_grad = zeros(size(Theta2));
@@ -46,12 +46,12 @@ Theta2_grad = zeros(size(Theta2));
 %         that your implementation is correct by running checkNNGradients
 %
 %         Note: The vector y passed into the function is a vector of labels
-%               containing values from 1..K. You need to map this vector into a 
+%               containing values from 1..K. You need to map this vector into a
 %               binary vector of 1's and 0's to be used with the neural network
 %               cost function.
 %
 %         Hint: We recommend implementing backpropagation using a for-loop
-%               over the training examples if you are implementing it for the 
+%               over the training examples if you are implementing it for the
 %               first time.
 %
 % Part 3: Implement regularization with the cost function and gradients.
@@ -61,6 +61,57 @@ Theta2_grad = zeros(size(Theta2));
 %               the regularization separately and then add them to Theta1_grad
 %               and Theta2_grad from Part 2.
 %
+
+
+%     1) Adding the 1's row to the X matrix
+X = [ones(m, 1) X]; % Dont forget to add your one's
+
+%     2) Converting vector 'y' to component (i think that's what it's called) matrix 'Y'
+
+rows_in_y = size(y, 1);
+Y = zeros(rows_in_y, num_labels); % Dimention 5000x10
+
+% Put a 1 in the proper row and column for the 1-9 + 0 digits
+for i = 1 : rows_in_y
+  Y(i, y(i)) = 1;
+end
+
+size(Y) % Dimention 5000x10, still, but now every row had a one in the column which represents the decimal value of it's y
+
+
+% 3)  calculate a2 - I dont really under stand where this comes from, i didnt understand it in the lecture.  I found it in a student's explanatin of how he solved it
+
+a2 = sigmoid(X * Theta1'); % a2 is 5000x25
+
+size(a2)
+
+% 4) calculate a3
+
+a2_ = [ones(m, 1) a2]; % Dont forget to add your one's
+
+a3 = sigmoid(a2_ * Theta2'); % 5000x10
+
+size(a3)
+
+% 4) calculate J
+h = a3;
+
+% There was a conversation about sum(sum()) and using matricies to solve this.  Appearently this solution is simplier, but slower.
+
+
+% Trevis, remember that no matter which matrix you transpose, the result is the same.  Just so happens that transposing Y results in a smaller matrix. (The sizes of the two products are 10x10, instead of 5000x5000)
+% J = 1/m * trace((-Y * log(h)' ) - (1 - Y) * log(1 - h)')
+
+J = 1/m * trace((-Y' * log(h) ) - (1 - Y)' * log(1 - h))
+
+% J = 1/m * sum(sum((-Y .* log(h) ) - (1 - Y) .* log(1 - h)))
+
+% The three above methods of calculating J give the same result.  Read this thing about the math to understand why: https://www.coursera.org/learn/machine-learning/discussions/all/threads/AzIrrO7wEeaV3gonaJwAFA
+
+
+
+
+
 
 
 
@@ -85,7 +136,8 @@ Theta2_grad = zeros(size(Theta2));
 % =========================================================================
 
 % Unroll gradients
-grad = [Theta1_grad(:) ; Theta2_grad(:)];
+
+% grad = [Theta1_grad(:) ; Theta2_grad(:)];
 
 
 end
